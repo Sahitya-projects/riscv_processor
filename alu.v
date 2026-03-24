097bbe2fefbc8fa7eb3ab0ac5f456df689a8c67d
Sahitya Boppana <srisahitya.boppana@gmail.com>
2026-03-24 22:23:10 +0530

    Add files via upload

---
+++ alu.v
@@ -0,0 +1,18 @@
+module alu(
+    input [31:0] a,
+    input [31:0] b,
+    input [2:0] alu_control,
+    output reg [31:0] result
+);
+
+always @(*) begin
+    case(alu_control)
+        3'b000: result = a + b;
+        3'b001: result = a - b;
+        3'b010: result = a & b;
+        3'b011: result = a | b;
+        default: result = 0;
+    endcase
+end
+
+endmodule
